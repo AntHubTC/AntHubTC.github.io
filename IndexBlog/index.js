91a5b9d1463052e65bef2125ad8ab375554d5224
AntHubTc <1096648786@qq.com>
2024-08-05 15:58:08 +0800

    jupyter notebook

--- IndexBlog/index.js
+++ IndexBlog/index.js
@@ -457,6 +457,12 @@ let docs = [
             "href": "../web3",
             "is_private": false, 
             "category": ["Web3"]
+         },{ 
+            "id": 81,
+            "title": "jupyter",
+            "href": "../jupyter",
+            "is_private": false, 
+            "category": ["Python"]
          }
 ];
 // 所有文档分类
@@ -657,7 +663,7 @@ let docCategory = [
 
 // 特殊定位符，前面是数据，后面是代码，python按最新数据重新拼接
 // >>>>>>>>>!@#$%^&*!<<<<<<<<<
-                                                                
+                                                                    
 // 防止抖动函数
 function debounce(func, delay) {
     let timerId;
---
+++ jupyter/install.md
@@ -0,0 +1,30 @@
+# 安装
+
+```bash
+pip install jupyter
+```
+
+**运行 Jupyter notebook 的命令：**
+
+```bash
+# 在当前目录运行jupter notebook
+jupyter notebook
+```
+
+默认打开：http://localhost:8888
+
+![image-20240805155004664](img/install/image-20240805155004664.png)
+
+## 其它使用命令
+
+```bash
+# 打开指定笔记本
+jupyter notebook notebook_name.ipynb
+# 指定启动端口
+jupyter notebook --port 9999
+# 在不打开 Web 浏览器的情况下启动笔记本服务器的命令：
+jupyter notebook --no-browser
+# 其它参数帮助查询
+jupyter notebook --help
+```
+
