ea75fbb8e7205a5d56139495e968ff7882961776
AntHubTC <tangcheng_java@foxmail.com>
2025-06-10 00:02:23 +0800

    张一鸣的231条blog

--- IndexBlog/index.js
+++ IndexBlog/index.js
@@ -531,10 +531,10 @@ let docs = [
             "category": ["读书"]
          },{ 
             "id": 93,
-            "title": "coze扣子",
-            "href": "../coze",
+            "title": "张一鸣的231条微博",
+            "href": "../231Blog",
             "is_private": false, 
-            "category": ["AI"]
+            "category": ["创业"]
          }
 ];
 // 所有文档分类
@@ -749,7 +749,7 @@ let docCategory = [
 
 // 特殊定位符，前面是数据，后面是代码，python按最新数据重新拼接
 // >>>>>>>>>!@#$%^&*!<<<<<<<<<
-                                                                                                                
+                                                                                                                    
                                                                                                     
 // 防止抖动函数
 function debounce(func, delay) {
